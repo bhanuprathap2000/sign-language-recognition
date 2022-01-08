now fixed the reshape issue by padding and masking

dataset.py
@@ -109,9 +109,37 @@ class  KeypointsDataset(data.Dataset):
 
         pose = (
             np.array(list(map(np.array, df.pose.values)))
-            .reshape(-1, 66)
             .astype(np.float32)
         )
+        #
+        pose_shape=pose.shape
+        total_frame=pose_shape[0]
+        # print("total frame:",total_frame)
+        temp_total_shape=total_frame
+        # print("temp total shape:",temp_total_shape)
+
+        while (temp_total_shape*pose_shape[1]*pose_shape[2])%50!=0:
+            temp_total_shape+=1
+        pose=np.concatenate([pose,np.zeros((temp_total_shape-total_frame,33,2))])
+        # print("shape of pose",pose.shape)
+
+        #
+        pose=pose.reshape(-1,50)
+        new_pose=pose.shape[0]
+        # print("new shape",new_pose)
+        # print("total frame",total_frame)
+
+        n_rows=0
+        
+        while new_pose*50>total_frame*50:
+            n_rows+=1
+            new_pose-=1
+        
+        # print("new pose",new_pose)
+        # print("n_rows",n_rows)
+        pose=pose[:-n_rows,:]
+        # print("pose shape",pose.shape)
+        # sys.exit(1)
         h1 = (
             np.array(list(map(np.array, df.hand1.values)))
             .reshape(-1, 42)

evaluate.py
@@ -13,10 +13,12 @@ from models import Transformer
 from configs import TransformerConfig
 from utils import load_json, load_label_map
 import shutil
-
+import os, sys
+import stat
 parser = argparse.ArgumentParser(description="Evaluate function")
 parser.add_argument("--data_dir", required=True, help="data directory")
 args = parser.parse_args()
+
 
 
 class KeypointsDataset(data.Dataset):
@@ -83,21 +85,61 @@ class KeypointsDataset(data.Dataset):
             }
         )
 
+
+ 
+
         pose = (
             np.array(list(map(np.array, df.pose.values)))
-            .reshape(-1, 50)
             .astype(np.float32)
         )
+        #
+        pose_shape=pose.shape
+        total_frame=pose_shape[0]
+        # print("total frame:",total_frame)
+        temp_total_shape=total_frame
+        # print("temp total shape:",temp_total_shape)
+
+        while (temp_total_shape*pose_shape[1]*pose_shape[2])%50!=0:
+            temp_total_shape+=1
+        pose=np.concatenate([pose,np.zeros((temp_total_shape-total_frame,33,2))])
+        # print("shape of pose",pose.shape)
+
+        #
+        pose=pose.reshape(-1,50)
+        new_pose=pose.shape[0]
+        # print("new shape",new_pose)
+        # print("total frame",total_frame)
+
+        n_rows=0
+        
+        while new_pose*50>total_frame*50:
+            n_rows+=1
+            new_pose-=1
+        
+        # print("new pose",new_pose)
+        # print("n_rows",n_rows)
+        pose=pose[:-n_rows,:]
+        # print("pose shape",pose.shape)
+        # sys.exit(1)
+        
+        
         h1 = (
             np.array(list(map(np.array, df.hand1.values)))
             .reshape(-1, 42)
             .astype(np.float32)
         )
+        # h1=np.concatenate([h1,np.zeros((pose.shape[0]-total_frame,21,2))])
+        # h1=h1.reshape(-1,42)
         h2 = (
             np.array(list(map(np.array, df.hand2.values)))
             .reshape(-1, 42)
             .astype(np.float32)
         )
+        # h2=np.concatenate([h2,np.zeros((pose.shape[0]-total_frame,21,2))])
+        # h2=h2.reshape(-1,42)
+        # print(pose.shape)
+        # print(h1.shape,h2.shape)
+        # sys.exit(1)
         final_data = np.concatenate((pose, h1, h2), -1)
         final_data = np.pad(
             final_data,
@@ -112,6 +154,9 @@ class KeypointsDataset(data.Dataset):
     def __len__(self):
         return len(self.files)
 
+# def del_evenReadonly(action, name, exc):
+#     os.chmod(name, stat.S_IWRITE)
+#     os.remove(name)
 
 @torch.no_grad()
 def inference(dataloader, model, device, label_map):
@@ -128,14 +173,19 @@ def inference(dataloader, model, device, label_map):
 
 
 video_paths = glob.glob(os.path.join(args.data_dir, "*"))
+print(video_paths)
+
 save_dir = "keypoints_dir"
 if os.path.isdir(save_dir):
     shutil.rmtree(save_dir)
 os.mkdir(save_dir)
+
+
 for path in tqdm(video_paths, desc="Processing Videos"):
     process_video(path, save_dir)
 
-label_map = load_label_map("include")
+label_map = load_label_map("include50")
+
 dataset = KeypointsDataset(
     keypoints_dir=save_dir,
     max_frame_len=169,
@@ -145,23 +195,23 @@ dataloader = data.DataLoader(
     dataset,
     batch_size=1,
     shuffle=False,
-    num_workers=4,
+    num_workers=0,
     pin_memory=True,
 )
 label_map = dict(zip(label_map.values(), label_map.keys()))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 config = TransformerConfig(size="large", max_position_embeddings=256)
-model = Transformer(config=config, n_classes=263)
+model = Transformer(config=config, n_classes=50)
 model = model.to(device)
 
-pretrained_model_name = "include_no_cnn_transformer_large.pth"
+pretrained_model_name = "include50_no_cnn_transformer_large.pth"
 pretrained_model_links = load_json("pretrained_links.json")
 if not os.path.isfile(pretrained_model_name):
     link = pretrained_model_links[pretrained_model_name]
     torch.hub.download_url_to_file(link, pretrained_model_name, progress=True)
 
-ckpt = torch.load(pretrained_model_name)
+ckpt = torch.load(pretrained_model_name,map_location=torch.device('cpu'))
 model.load_state_dict(ckpt["model"])
 print("### Model loaded ###")
 